5c0a57afb322d40ecb0e7af0004f884275013045
[Mark Hallam] <[nippermh@hotmail.com]>
2021-09-26 13:58:22 +1000

    setting up  tracks in latest tracks section

--- src/components/LatestTracks/LatestTracksElements.js
+++ src/components/LatestTracks/LatestTracksElements.js
@@ -1,25 +1,83 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const LatestTracksContainer = styled.div`
-    color: #fff;
-    background: ${({lightBg}) => (lightBg ? '#f9f9f9' : '#010606')}
-;
+    height: 800px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: #010606;
 
-@media screen and (max-width: 768px) {
-    padding: 100px 0;
-}
+    @media screen and (max-width: 768px) {
+        height: 1100px;
+    }
 
+    @media screen and (max-width: 480px) {
+        height: 1300px;
+    }
 `;
 
 export const LatestTracksWrapper = styled.div`
+    max-width: 1000px;
+    margin: 0 auto;
     display: grid;
-    z-index: 1;
-    height 860px;
-    width: 100%;
-    max-width: 1100px;
-    margin-right: auto;
-    margin-left: auto;
-    padding 0 24px;
-    justify-content: center;
+    grid-template-columns: 1fr 1fr 1fr;
+    align-items: center;
+    grid-gap: 16px;
+    padding: 0 50px;
 
+    @media screen and (max-width: 1000px) {
+        grid-template-columns: 1fr 1fr;
+    }
+
+    @media screen and (max-width: 768px) {
+        grid-template-columns: 1fr;
+        padding: 0 20px;
+    }
+`;
+
+export const LatestTracksCard = styled.div`
+    background: #fff;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    border-radius: 10px;
+    max-height: 340px;
+    padding: 30px;
+    box-shadow: 0 1px 3px rgba(0,0,0,0.2);
+    transition: all 0.2s ease-in-out;
+
+    &:hover {
+        transform: scale(1.02);
+        transition: all 0.2s ease-in-out;
+        cursor: pointer;
+    }
+`;
+
+export const LatestTracksIcon = styled.img`
+    height: 160px;
+    width: 160px;
+    margin-bottom: 10px;
+
+`;
+
+export const LatestTracksH1 = styled.h1`
+    font-size: 2.5rem;
+    color: #fff;
+    margin-bottom: 64px;
+
+    @media screen and (max-width: 480px) {
+        font-size: 2rem;
+    }
+`;
+
+export const LatestTracksH2 = styled.h2`
+    font-size: 1rem;
+    margin-bottom: 10px;
+`;
+
+export const LatestTracksP = styled.p`
+    font-sizes: 1rem;
+    text-align: center;
 `;
--- src/components/LatestTracks/index.js
+++ src/components/LatestTracks/index.js
@@ -2,17 +2,65 @@ import React from 'react';
 import ReactPlayer from 'react-player';
 import {
     LatestTracksContainer,
-    LatestTracksWrapper
+    LatestTracksWrapper,
+    LatestTracksCard,
+    LatestTracksH2,
+    LatestTracksP
   
 } from './LatestTracksElements';
 
-const LatestTracks = ({lightBg, id}) => {
+const LatestTracks = () => {
     return (
         <>
-           <LatestTracksContainer lightBg={lightBg} id={id}>
+           <LatestTracksContainer>
               <LatestTracksWrapper>
-                  this is some text
-                    <ReactPlayer url= {'https://soundcloud.com/adrianhallam/telegram'} />
+
+                  <LatestTracksCard>
+                  
+                    <LatestTracksH2>Reduce Expenses</LatestTracksH2>
+                    <LatestTracksP>We help you to reduce your bank fees and increase your overall revenue.</LatestTracksP>
+                
+                    <ReactPlayer 
+                        url= {'https://soundcloud.com/adrianhallam/telegram'} 
+                        width='100%'
+                        height='100%'
+                        
+                        />
+
+                    </LatestTracksCard>
+
+                    <LatestTracksCard>
+                 
+                    <LatestTracksH2>Telegram</LatestTracksH2>
+                    <LatestTracksP>An ambient guitar instrumental created on my new Telecaster:)</LatestTracksP>
+                
+                    <ReactPlayer 
+                        url= {'https://soundcloud.com/adrianhallam/telegram'} 
+                        width='100%'
+                        height='100%'
+                        
+                        />
+
+                    </LatestTracksCard>
+
+                    <LatestTracksCard>
+                 
+                    <LatestTracksH2>Space Out</LatestTracksH2>
+                    <LatestTracksP>I recorded this when covid kicked off, last year here in Oz.
+
+Basically just turned government messaging into the lyrics of the song.
+At first i thought it kinda sucked and didn't bother releasing it.
+But after adjusting the mix and playing around with the arrangement i've grown a lot more fond of it.
+Hope you enjoy, and thanks for listening/feedback :)</LatestTracksP>
+                
+                    <ReactPlayer 
+                        url= {'https://soundcloud.com/adrianhallam/space-out'} 
+                        width='100%'
+                        height='100%'
+                        
+                        />
+
+                    </LatestTracksCard>
                 </LatestTracksWrapper> 
             </LatestTracksContainer>
         </>
